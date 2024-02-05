d017d666f64e594994060ed58bf445d2094e60a6
Yilbert Luna <yilbert.luna.e@gmail.com>
2024-02-05 16:05:14 -0400

    refactor code to input search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,82 +1,21 @@
 'use client'
-import { useState, useEffect, useRef } from "react";
-import { userInfo } from "@/interfaces/interfaces";
-import { CiSearch  } from "react-icons/ci";
-import { MdDarkMode , MdLightMode } from "react-icons/md";
+import { useState } from "react";
+import { userInfo } from "../interfaces/interfaces";
+import InputSearch from "../components/inputSearch";
 
 export default function Home() {
 
-  const [user, setUser] = useState<userInfo>()
-  const [notfound, setNotfound] = useState<boolean>(false)
-  const [empiti, setEmpiti] = useState<boolean>(false)
-  const [inputValue, setInputValue] = useState<string>('octocat')
-  const userNameRef = useRef<HTMLInputElement>(null)
-  
+  const [user, setUser] = useState<userInfo | null>(null)
 
-  // function to get input value and set input value
-  function submitInputValue() {
-    if(userNameRef.current?.value.trim() === "" || userNameRef.current?.value === undefined){
-      setEmpiti(true)
-      setUser(undefined)
-      return
-    }
-    setEmpiti(false)
-    setInputValue(String(userNameRef.current?.value))
+
+  function setUserData(user: userInfo | null): void {
+    setUser(user);
   }
-
-  // effect to change the user when the input value is changed
-  useEffect(() => {
-
-    async function getDataUser() {
-      const response = await fetch(`https://api.github.com/users/${inputValue}`);
-      const data = await response.json();
-  
-      if (response.status !== 200) {
-        setNotfound(true);
-        setUser(undefined);
-        return;
-      }
-  
-      setNotfound(false);
-      setUser(data);
-    }
-
-    getDataUser()
-
-  }, [inputValue])
-
-
 
   return (
     <div className="flex flex-col justify-center items-center">
       {/* input */}
-      <div className="flex flex-col justify-center items-center">
-        <div className="flex flex-row justify-between w-full mt-32">
-          <h1>DEVFINDER</h1>
-          <p className="flex flex-row justify-center items-center gap-1">
-            <span>
-              dark
-            </span>
-            <MdDarkMode  className="text-xl text-gray-500"/>
-            {/* <MdLightMode className="text-xl text-gray-500"/> */}
-          </p>
-        </div>
-        <div className="flex justify-center items-center mt-5 w-80 p-5 bg-white shadow-inputShadow rounded-lg">
-          <form onSubmit={e => {
-            e.preventDefault()
-            submitInputValue()
-          }} className="flex flex-row justify-center items-center gap-2">
-            <CiSearch className="text-3xl"/>
-        
-            <input ref={userNameRef} type="text" name="username" id="username" placeholder="search github username..." className="placeholder-black focus:outline-none w-40 bg-white"/>
-
-            {empiti && <p className="text-red-500 text-xs">Enter User</p>}
-            {notfound && <p className="text-red-500 text-xs">Not Fount</p>}
-
-            <button type="submit" className="bg-button text-white p-2 rounded-lg">search</button>
-          </form>
-        </div>
-      </div>
+      <InputSearch setUser={setUserData}/>
       
       {/* user Info */}
       <div className="mt-20">
---
+++ src/components/inputSearch/index.tsx
@@ -0,0 +1,80 @@
+'use client'
+import { useState, useEffect, useRef } from "react"
+import { CiSearch  } from "react-icons/ci";
+import { userInfo } from "../../interfaces/interfaces";
+import { MdDarkMode , MdLightMode } from "react-icons/md";
+
+type props = {
+    setUser: (user: userInfo | null) => void;
+}
+
+export default function InputSearch({setUser}: props) {
+
+    const [notfound, setNotfound] = useState<boolean>(false)
+    const [empiti, setEmpiti] = useState<boolean>(false)
+    const userNameRef = useRef<HTMLInputElement>(null)
+    const [inputValue, setInputValue] = useState<string>('octocat')
+
+    function submitInputValue() {
+        if(userNameRef.current?.value.trim() === "" || userNameRef.current?.value === undefined){
+          setEmpiti(true)
+          setUser(null)
+          return
+        }
+        setEmpiti(false)
+        setInputValue(String(userNameRef.current?.value))
+    }
+
+    // effect to change the user when the input value is changed
+    useEffect(() => {
+
+        async function getDataUser() {
+        const response = await fetch(`https://api.github.com/users/${inputValue}`);
+        const data = await response.json();
+    
+        if (response.status !== 200) {
+            setNotfound(true);
+            setUser(null);
+            return;
+        }
+    
+        setNotfound(false);
+        setUser(data);
+        }
+
+        getDataUser()
+
+    }, [inputValue, setUser])
+    
+
+    return(
+        <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-row justify-between w-full mt-32">
+                <h1>DEVFINDER</h1>
+                <p className="flex flex-row justify-center items-center gap-1">
+                    <span>
+                    dark
+                    </span>
+                    <MdDarkMode  className="text-xl text-gray-500"/>
+                    {/* <MdLightMode className="text-xl text-gray-500"/> */}
+                </p>
+            </div>
+            <div className="flex justify-center items-center mt-5 w-80 p-5 bg-white shadow-inputShadow rounded-lg">
+                <form onSubmit={e => {
+                    e.preventDefault()
+                    submitInputValue()
+                }} className="flex flex-row justify-center items-center gap-2">
+                    <CiSearch className="text-3xl"/>
+                
+                    <input ref={userNameRef} type="text" name="username" id="username" placeholder="search github username..." className="placeholder-black focus:outline-none w-40 bg-white"/>
+                    {/* <input type="text" name="username" id="username" placeholder="search github username..." className="placeholder-black focus:outline-none w-40 bg-white"/> */}
+
+                    {empiti && <p className="text-red-500 text-xs">Enter User</p>}
+                    {notfound && <p className="text-red-500 text-xs">Not Fount</p>}
+
+                    <button type="submit" className="bg-button text-white p-2 rounded-lg">search</button>
+                </form>
+            </div>
+      </div>
+    )
+}
