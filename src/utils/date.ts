ad3a4efca12096735043363713ba0740c2af302c
Yilbert Luna <yilbert.luna.e@gmail.com>
2024-02-08 23:33:33 -0400

    modified file

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 export function dateCreated(DATE: string | undefined): string {
     const date = dayjs(DATE)
 
-    const formate = `Joined ${date.format("DD/MM/YYYY")}`
+    const formate = `Joined ${date.format("DD MMM YYYY")}`
 
     return formate
 }
